e8869cc9a9fb7602cfa69612c3158aaad63a2464
Alex <fff.alex.er.fff@gmail.com>
2025-08-22 17:03:06 +0600

    fix: jedi-server has been deleted, now the main linter is pyright

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -2,7 +2,7 @@
 local lspconfig = require "lspconfig"
 local lsp_defaults = require("nvchad.configs.lspconfig").defaults() or {}
 
-local servers = { "pyright", "jedi_language_server", "html", "cssls" }
+local servers = { "pyright", "html", "cssls" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup(vim.tbl_deep_extend("force", lsp_defaults, {}))
